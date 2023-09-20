8d26a8884c05631cac60337df5a3ca2152cb9479
Ivan Dolgov <idlhero@gmail.com>
2023-09-20 23:59:01 +0300

    Refactor formatter

--- app/lib/linter/formatter.rb
+++ app/lib/linter/formatter.rb
@@ -11,22 +11,22 @@ module Linter
       case @linter.check.repository.language
       when :javascript
         @issues_count = count_issues_eslint(parsed_raw_data)
-        formatted_data = format_eslint_json(parsed_raw_data)
+        formatted_data = format_eslint(parsed_raw_data)
         @json_data = JSON.generate(formatted_data)
       when :ruby
         @issues_count = count_issues_rubocop(parsed_raw_data)
-        formatted_data = format_rubocop_json(parsed_raw_data)
+        formatted_data = format_rubocop(parsed_raw_data)
         @json_data = JSON.generate(formatted_data)
       end
     end
 
     private
 
-    def format_eslint_json(json)
+    def format_eslint(data)
       return {} if @issues_count.zero?
 
       repository_directory = @linter.check.repository.decorate.directory_path
-      json.map do |file|
+      data.map do |file|
         issues = file[:messages].map do |message|
           {
             message: message[:message],
@@ -39,11 +39,11 @@ module Linter
       end
     end
 
-    def format_rubocop_json(json)
+    def format_rubocop(data)
       return {} if @issues_count.zero?
 
       repository_directory = @linter.check.repository.decorate.directory_path
-      json[:files].map do |file|
+      data[:files].map do |file|
         issues = file[:offenses].map do |offense|
           {
             message: offense[:message],
@@ -56,12 +56,12 @@ module Linter
       end
     end
 
-    def count_issues_eslint(json)
-      json.reduce(0) { |sum, file| sum + file[:messages].count }
+    def count_issues_eslint(data)
+      data.reduce(0) { |sum, file| sum + file[:messages].count }
     end
 
-    def count_issues_rubocop(json)
-      json[:summary][:offense_count]
+    def count_issues_rubocop(data)
+      data[:summary][:offense_count]
     end
   end
 end
