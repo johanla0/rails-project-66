8f033c846c1d0bf0cc70b70bf51194d19c7054b7
Ivan Dolgov <idlhero@gmail.com>
2023-10-02 17:01:26 +0300

    Apply linter

--- config/routes.rb
+++ config/routes.rb
@@ -28,8 +28,8 @@ Rails.application.routes.draw do
       # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
       # - Use & (do not use &&) so that it doesn't short circuit.
       # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(Rails.application.credentials.dig(:sidekiq, :username))) &
-        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(Rails.application.credentials.dig(:sidekiq, :password)))
+      ActiveSupport::SecurityUtils.secure_compare(Digest::SHA256.hexdigest(username), Digest::SHA256.hexdigest(Rails.application.credentials.dig(:sidekiq, :username))) &
+        ActiveSupport::SecurityUtils.secure_compare(Digest::SHA256.hexdigest(password), Digest::SHA256.hexdigest(Rails.application.credentials.dig(:sidekiq, :password)))
     end
     mount Sidekiq::Web, at: '/sidekiq'
   end
