24415578496fa198fb76814ea941724c310ec9d3
Ivan Dolgov <idlhero@gmail.com>
2023-10-04 13:31:06 +0300

    Fix policies

--- app/controllers/web/repositories/checks_controller.rb
+++ app/controllers/web/repositories/checks_controller.rb
@@ -3,15 +3,15 @@
 class Web::Repositories::ChecksController < Web::Repositories::ApplicationController
   def show
     repository = Repository.find params[:repository_id]
-    authorize repository
+    authorize repository, policy_class: Repository::CheckPolicy
 
     @check = Repository::Check.find params[:id]
     @issues = JSON.parse(@check.issues, symbolize_names: true) if @check.issues.present?
   end
 
   def create
-    repository = current_user.repositories.find(params[:repository_id])
-    authorize repository
+    repository = current_user.repositories.find params[:repository_id]
+    authorize repository, policy_class: Repository::CheckPolicy
 
     if RepositoryService.check!(repository)
       f :success, redirect: repositories_path
--- app/policies/repository/check_policy.rb
+++ app/policies/repository/check_policy.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
-class Repository
-  class CheckPolicy < ApplicationPolicy
-    def show?
-      user_is_owner?
-    end
+class Repository::CheckPolicy < ApplicationPolicy
+  def show?
+    user_is_owner?
+  end
 
-    def create?
-      user_is_owner?
-    end
+  def create?
+    user_is_owner?
+  end
 
-    def user_is_owner?
-      user == record.repository.user
-    end
+  def user_is_owner?
+    user == record.user
   end
 end
