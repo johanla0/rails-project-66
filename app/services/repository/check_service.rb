91c189a06cd94c442a737bf92311366416a78abb
Ivan Dolgov <idlhero@gmail.com>
2023-10-17 22:32:39 +0300

    Update check service

--- app/services/repository/check_service.rb
+++ app/services/repository/check_service.rb
@@ -3,17 +3,11 @@
 class Repository::CheckService
   class << self
     def shallow_clone!(repository)
-      FileUtils.mkdir_p(repository.directory_path)
-      Dir.chdir(repository.directory_path)
-
-      begin
-        Git.clone(repository.clone_url, nil, depth: 1)
-        # Git.clone(repository.clone_url, repository.directory_path, depth: 1)
-      rescue Git::FailedError => e
-        Rails.logger.error(e.message)
-        Sentry.capture_exception(e)
-        nil
-      end
+      Git.clone(repository.clone_url, repository.directory_path, depth: 1)
+    rescue Git::FailedError => e
+      Rails.logger.error(e.message)
+      Sentry.capture_exception(e)
+      nil
     end
   end
 end
